b209ff64bacbfc4e926cf9a3b46f0ae398becaca
Sergey Portnoy <ihomelleoni@yandex.ru>
2021-11-02 13:35:43 +0300

    MBLA Preparing Added heads category

---
+++ Source/Script/CodeDB/CSharp/Generators/Heads_CSharp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UBlockly
+{
+    public partial class CSharpGenerator
+    {
+        [CodeGenerator(BlockType = "heads_set")]
+        private CodeStruct Heads_Set(Block block)
+        {
+            Debug.Log("HEADS SET GENERATOR");
+            string code = block.GetFieldValue("ANGLE");
+            float num = float.Parse(code);
+            int order = num < 0 ? CSharp.ORDER_UNARY : CSharp.ORDER_ATOMIC;
+            return new CodeStruct(code, order);
+        }
+    }
+}
---
+++ Source/Script/CodeDB/CSharp/Interpreters/Heads_CSharp.cs
@@ -0,0 +1,29 @@
+﻿
+using Mainbot.Winky.API.Actuators;
+using UnityEngine;
+using WinkyApp;
+using WinkyApp.LabAdvanced;
+
+namespace UBlockly
+{
+    public partial class CSharpGenerator
+    {
+        
+        [CodeInterpreter(BlockType = "heads_set")]
+        public class AngleCmdtor : ValueCmdtor
+        {
+            protected override DataStruct Execute(Block block)
+            {
+                var angle = int.Parse(block.GetFieldValue("ANGLE"));
+                Debug.Log("VALUE: " + angle);
+                var neckMsg = Neck.Rotate((short) angle);
+                var runner = ServiceLocator.GetService<IAdvancedProgramRunner>();
+                runner.AddMessage(neckMsg);
+                runner.Run();
+                Number num = new Number(angle);
+                return new DataStruct(num);
+            }
+        }
+
+    }
+}
